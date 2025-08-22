c80ff393597a32837e29a423a84d185d71a0142b
= <Arielh9001@gmail.com>
2025-08-22 02:26:55 -0400

    Better formatted Interface system

--- interface.py
+++ interface.py
@@ -1,9 +1,10 @@
 import pygame
 
 class UIObj():
-  def __init__(self, name = 'NONAME'):
+  def __init__(self, name = 'UIOBJECT'):
     self.children = {}
     self.name     = name
+    self.visible  = True
   
   def __call__(self, child = ''):
     if child in self.children:
@@ -15,7 +16,6 @@ class UIObj():
   def render(self, surface):
     if surface:
       self.loop('render', surface)
-    print(self.name, surface)
   
   def input(self):
     self.loop('input')
@@ -23,23 +23,36 @@ class UIObj():
   def loop(self, call:str, *values:any):
     if not call: return
     for item in self.children.values():
-      if hasattr(item, call):
-        getattr(item, call)(*values)
+      if hasattr(item, call) and hasattr(item, 'visible'):
+        att = getattr(item, call)
+        if callable(att) and getattr(item, 'visible'):
+          att(*values)
+        
   
-  def add(self, name:str, obj:any = None):
+  def add(self, name:str, obj:type = None):
     if not name:
       name = f'Frame-{len(self.children) + 1}'
-    if not obj:
+    if (type(obj) == type):
+      obj = obj(name)
+    else:
       obj = Frame(name)
     self.children[name] = obj
     return obj
   
-#
+
+# ---------------------------------
 class Frame(UIObj):
-  def __init__(self, name=''):
+  def __init__(self, name = ''):
     super().__init__(name)
-    self.visible  = False
     self.position = (0,0)
+    self.size     = (10,10)
+    self.color    = (255,255,255)
+    self.rotation = 0
+    # --
+    
+
+  def add(self, name, obj):
+    return super().add(name, obj)
 
   def update(self, tick = 0, localTime = 0):
     super().update(tick, localTime)
@@ -50,22 +63,5 @@ class Frame(UIObj):
   def input(self):
     super().input()
 
-  def add(self, name):
-    return super().add(name)
-# ----
-class UIManager(UIObj):
-  def __init__(self, name = 'UIManager'):
-    return super().__init__(name)
-  
-  def add(self, name):
-    return super().add(name)
-  
-  def update(self, tick = 0, localTime = 0):
-    super().update(tick, localTime)
-  
-  def render(self, surface):
-    super().render(surface)
-  
-  def input(self):
-    super().input()
+# ---------------------------------
 
